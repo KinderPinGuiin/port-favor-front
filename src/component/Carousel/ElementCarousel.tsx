60e4e22f3fb187f43cce41f47f5099f8c2595ac7
jordanredlab <jordan.elie@redlab.io>
2023-12-27 18:27:01 +0100

    feat: Added carousel

---
+++ src/component/Carousel/ElementCarousel.tsx
@@ -0,0 +1,63 @@
+import { Box, SxProps, Theme } from "@mui/material";
+import { config } from "@react-spring/web";
+import { useMemo, useState } from "react";
+import { ReactElement } from "react";
+import Carousel from "react-spring-3d-carousel";
+
+/**
+ * Contains all the props of a Carousel component.
+ */
+export type CarouselProps = {
+  /**
+   * The elements to place inside the carousel.
+   */
+  children: ReactElement[];
+
+  /**
+   * Indicates if navigation arrows has to be shown under the carousel (default: false).
+   */
+  showNavigation?: boolean;
+
+  /**
+   * The carousel container style to apply.
+   */
+  style?: SxProps<Theme>;
+};
+
+/**
+ * An image carousel that allows you to slide elements to visualize them.
+ */
+export default function ElementCarousel({
+  children,
+  style,
+  showNavigation,
+}: CarouselProps) {
+  // Check the props
+  if (children.length < 3) {
+    throw "A carousel needs at least 3 elements.";
+  }
+
+  const [goToSlide, setGoToSlide] = useState(0);
+
+  // Add a max width and max height property to the childrens style (so they don't overflow their container)
+  const enhancedChildrens = useMemo(
+    () =>
+      children.map((c, i) => ({
+        key: `${i}`,
+        content: c,
+        onClick: () => setGoToSlide(i),
+      })),
+    []
+  );
+
+  return (
+    <Box sx={style}>
+      <Carousel
+        slides={enhancedChildrens}
+        goToSlide={goToSlide}
+        showNavigation={showNavigation ?? false}
+        animationConfig={config.gentle}
+      />
+    </Box>
+  );
+}
--- src/page/portfolio/Portfolio.tsx
+++ src/page/portfolio/Portfolio.tsx
@@ -7,22 +7,39 @@
  * - Adapter les options d'api pour customiser le retour (pas seulement json : binaire)
  */
 
-import Carousel from "@component/Carousel/Carousel";
+import ElementCarousel from "@component/Carousel/ElementCarousel";
 import CenterDiv from "@component/CenterDiv/CenterDiv";
 
 export default function Portfolio() {
   const images = [
-    { src: "https://picsum.photos/500", alt: "Image 1" },
-    { src: "https://picsum.photos/500", alt: "Image 2" },
-    { src: "https://picsum.photos/500", alt: "Image 3" },
+    { src: "https://picsum.photos/800", alt: "Image 1" },
+    { src: "https://picsum.photos/900", alt: "Image 2" },
+    { src: "https://picsum.photos/1200", alt: "Image 3" },
   ];
 
   return (
-    <CenterDiv sx={{ height: "100vh" }}>
-      <Carousel style={{ height: "70%", width: "90%" }} >
-        {images.map(image => (<img src={image.src} alt={image.alt} />))}
-        {/* {images.map((_, i) => (<h2>Test {i}</h2>))} */}
-      </Carousel>
-    </CenterDiv>
-  )
+    <>
+      {/* Last publication section */}
+      <section>
+        <h1
+          style={{
+            textAlign: "center",
+            height: "10vh",
+            margin: 0,
+            paddingTop: "30px",
+            fontSize: "2.5em",
+          }}
+        >
+          Nos dernières publications
+        </h1>
+        <CenterDiv sx={{ height: "90vh" }}>
+          <ElementCarousel style={{ height: "80%", width: "60%" }}>
+            {images.map((image, i) => (
+              <img src={image.src} alt={image.alt} key={i} />
+            ))}
+          </ElementCarousel>
+        </CenterDiv>
+      </section>
+    </>
+  );
 }
