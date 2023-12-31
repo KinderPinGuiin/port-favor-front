e176833ba48c5ddd9b35b70e8ed8953458bc82a3
Neuskan <94790506+Neuskan@users.noreply.github.com>
2023-12-31 19:56:49 +0100

    fix: problem with download button

--- src/component/ImageCard/ImageCard.tsx
+++ src/component/ImageCard/ImageCard.tsx
@@ -1,8 +1,9 @@
-import { Button, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
-// import { useTheme } from "@mui/material";
+import { useTheme } from "@mui/material";
 import { useDownloadImage } from "@hook/image/useDownloadImage";
 import { Snackbar } from '@mui/material';
+import { IconButton } from '@mui/material';
 
 export type ImageCardProps = {
   image: {
@@ -13,7 +14,7 @@ export type ImageCardProps = {
 };
 
 export default function ImageCard({ image }: ImageCardProps) {
-  // const theme = useTheme();
+  const theme = useTheme();
   const { src, name, description } = image;
   const defaultName = "Nom de l'image";
   const defaultDescription = "Description de l'image Description de l'imageDescription de l'image";
@@ -32,7 +33,7 @@ export default function ImageCard({ image }: ImageCardProps) {
        />
     <Card>
       <Grid container>
-        <Grid item xs={9}>
+        <Grid item xs={8}>
           <CardMedia
             component="img"
             image={src}
@@ -44,23 +45,17 @@ export default function ImageCard({ image }: ImageCardProps) {
             }}
           />
         </Grid>
-        <Grid item xs={3} overflow="auto">
+        <Grid item xs={4} overflow="auto">
           <CardContent>
             <Typography variant="h5" component="div" style={{ paddingBottom: "10px" }}>
               {name || defaultName}
             </Typography>
-            <Typography variant="subtitle1" component="div" color="text.secondary">
+            <Typography variant="subtitle1" component="div" color="text.secondary" style={{ paddingBottom: "10px"}}>
               {description || defaultDescription}
             </Typography>
-            <Button variant="contained" startIcon={<DownloadIcon style={{ fontSize: "2rem", cursor: "pointer"}}/>}
-              onClick={handleDownload}
-              style={{
-                position: 'absolute',
-                bottom: 1,
-                right: 1
-              }}
-            >Télécharger
-            </Button>
+            <IconButton aria-label="Télécharger l'image" sx={{ color: theme.palette.primary.light }}>
+            <DownloadIcon onClick={handleDownload}/>
+            </IconButton>
           </CardContent>
         </Grid>
       </Grid>
